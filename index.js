272686864b7cd02b80c5f8e330f33cfe6efd1022
AlejandraSaldana <A00838889@tec.mx>
2026-04-13 21:23:57 -0600

    fcb proxy server

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*" // allows any frontend to call it
+}));
 
 // RSS proxy
 app.get("/api/rss/cat/:category", async (req, res) => {
